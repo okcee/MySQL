92458c1c20753c415dc0b4fb5b48ac685a18b352
David Saavedra Varela <okcee@hotmail.com>
2025-04-08 23:55:13 +0200

    d080425 .4

--- Flask/app.py
+++ Flask/app.py
@@ -1,14 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+titulo_app = 'Zona Fit (GYM)'
 
 @app.route('/')  # url: http://localhost:5000/
 @app.route('/index.html') # url: http://localhost:5000/index.html
 
 def inicio():
     app.logger.debug('Entramos al path de inicio /')
-    return '<p>Hola Mundo</p>'
+    return render_template('index.html', titulo=titulo_app)
 
 if __name__ == '__main__':
     app.run(debug=True)
