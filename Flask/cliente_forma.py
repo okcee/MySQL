76cb4d1da705bd204c18b83c91233bb66281cba3
David Saavedra Varela <okcee@hotmail.com>
2025-04-10 11:33:13 +0200

    d100425 .1

--- Flask/cliente_forma.py
+++ Flask/cliente_forma.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms.fields.simple import StringField, SubmitField
+from wtforms.fields.simple import StringField, SubmitField, HiddenField
 from wtforms.fields.numeric import IntegerField
 from wtforms.validators import DataRequired
 
-
 class ClienteForma(FlaskForm):
+    id = HiddenField('id')
     nombre = StringField('Nombre', validators=[DataRequired()])
     apellido = StringField('Apellido', validators=[DataRequired()])
     membresia = IntegerField('Membresia', validators=[DataRequired()])
